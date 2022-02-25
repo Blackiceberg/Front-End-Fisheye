202710a460541c6a39de78f87bc1c583be05ad64
Blackiceberg <theblackiceberg@gmail.com>
2022-02-25 15:13:34 +0100

    Update FotographerFactory

--- assets/scripts/factories/photographer.js
+++ assets/scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait,city,country,tagline,price } = data;
 
     const picture = `assets/images/photographers/${portrait}`;
 
@@ -7,10 +7,23 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        
+        const h2 = document.createElement ('h2');
+        const h3 = document.createElement ('h3');
+        const h4 = document.createElement ('h4');
+        const h5 = document.createElement ('h5');
+        
         h2.textContent = name;
+        h3.textContent = city + ', ' + country;
+        h4.textContent = tagline;
+        h5.textContent = price + '€/Jour';
+        
         article.appendChild(img);
         article.appendChild(h2);
+        article.apprendChild(h3);
+        article.apprendChild(h4);
+        article.apprendChind(h5);
+        
         return (article);
     }
     return { name, picture, getUserCardDOM }
