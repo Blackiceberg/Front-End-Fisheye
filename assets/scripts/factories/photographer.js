6820a085825283768ac97398bceec6fecf18fd07
Blackiceberg <theblackiceberg@gmail.com>
2022-03-01 22:15:15 +0100

    Titre page interieur

--- assets/scripts/factories/photographer.js
+++ assets/scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
     const { name, portrait,city,country,tagline,price, id } = data;
     const picture = `assets/images/photographers/accounts/${portrait}`;
-    const pageInterieur = `/html/photographer.html`
+    //const pageInterieur = `/html/photographer.html`
     
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -31,20 +31,35 @@ function photographerFactory(data) {
 //page interieur
 function remove () {
 const index = document.getElementById('index');
+const TITRE = document.getElementById('TITRE');
+TITRE.remove();
+
 index.remove();
 
 };
 
 function creatPage (){
+  const picture = `assets/images/photographers/accounts/${portrait}`;
   const pageInterieur = document.createElement('div');
+  pageInterieur.setAttribute('id', 'profil');
   const profil = document.createElement('article');
   profil.classList.add("photographer_profile");
   document.getElementById('main').appendChild(pageInterieur);
   pageInterieur.classList.add("photograph-header");
+  
+
+  
   pageInterieur.appendChild(profil);
   const h1 = document.createElement ('h1');
   const p = document.createElement ('p');
   const small = document.createElement('small');
+  const button = document.createElement ('button');
+  button.classList.add("contact_button")
+  button.setAttribute('onclick','displayModal()')
+  button.innerHTML = `Contactez-moi`;
+
+
+
 
   h1.textContent = name;
   p.textContent = city + ', ' + country;
@@ -53,7 +68,12 @@ function creatPage (){
   profil.appendChild(h1);
   profil.appendChild(p);
   profil.appendChild(small);
+  pageInterieur.appendChild(button);
+  console.log(pageInterieur);
+  console.log(profil);
+  pageInterieur.appendChild(img);
 
+  //window.location.href = "html/photographer.html";
 
 };
 
