0a3ac3badf50de4f3cd3710d822d34fa6402bac0
Blackiceberg <theblackiceberg@gmail.com>
2022-03-01 19:47:55 +0100

    Page_interieur.js

--- assets/scripts/pages/index.js
+++ assets/scripts/pages/index.js
@@ -1,6 +1,4 @@
 
-const api_url = 'https://blackiceberg.github.io/Front-End-Fisheye/assets/scripts/data/photographers.json';
-
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
@@ -11,12 +9,4 @@ async function displayData(photographers) {
     });
 };
 
-async function init() {
-
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const {photographers, media } = data;
-    displayData(photographers);
-
-};
 init();
--- assets/scripts/pages/photographer.js
+++ assets/scripts/pages/photographer.js
@@ -1,1 +1,15 @@
 //Mettre le code JavaScript lié à la page photographer.html
+init();
+
+
+
+
+async function profilData(photographersProfile) {
+    const photographersHeader = document.querySelector(".photographer_profile");
+
+    photographersID.forEach((photographer) => {
+        const photographerModel = photographersProfile(photographer);
+        const userCardDOMId = photographerModel.getUserCardDOMId();
+        photographersHeader.appendChild(userCardDOMId);
+    });
+};
