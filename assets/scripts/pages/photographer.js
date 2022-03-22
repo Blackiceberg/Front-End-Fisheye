91e7cfcbb2f69c222248a9bc95d9aef1d1c6a246
Blackiceberg <theblackiceberg@gmail.com>
2022-03-22 15:59:22 +0100

    Photographer

--- assets/scripts/pages/photographer.js
+++ assets/scripts/pages/photographer.js
@@ -1,23 +1,37 @@
+(async function(){
+    const photographerId = getPhotographerId()
+    const photographers = await getPhotographer(photographerId)
+
+    console.log(photographers)
+    console.log(photographerId)
+    console.log(photographers[0].id)
+    hydrateProfilPhotographer(photographers)
+})()
+
+function getPhotographerId(){
+    return new URL(location.href).searchParams.get("id")
+}
+
+
+async function getPhotographer(photographerId){
+    return fetch ("https://blackiceberg.github.io/Front-End-Fisheye/assets/scripts/data/photographers.json")
+    .then(function(httpBodyResponse){
+        return httpBodyResponse.json()   
+    })
+    .then(function(data){
+        return data.photographers
+    })
+    .catch(function(error){
+        alert(error)
+    })
+}
+
+
+function hydrateProfilPhotographer(photographer){
+
+}
+
 
 //Effacer titre 
 document.getElementById('TITRE').style.opacity="0";
-
-(async function () {
-    const photographerID = getphotographerId()
-    console.log(photographerID)
-    const photographer = getphotographer(photographerID)
-    hydratePhotographer(photographer)
-
-})()
-
-function getphotographerId(){
-   return new URL(location.href).searchParams.get("id")
-} 
-
-function getphotographer(photographerID){
-
-}
-
-function hydratePhotographer(photographer){
-
-}
+document.getElementById('main');
